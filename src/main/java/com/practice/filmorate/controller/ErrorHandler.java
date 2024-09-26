6c4cd26896a7d7f326bb09f6f7e0054b3bf22dd9
LamoureuseCB <148466239+LamoureuseCB@users.noreply.github.com>
2024-09-26 11:05:24 +0500

    added ErrorHandler and Mapping menthods

---
+++ src/main/java/com/practice/filmorate/controller/ErrorHandler.java
@@ -0,0 +1,30 @@
+package com.practice.filmorate.controller;
+
+import com.practice.filmorate.exceptions.NotFoundException;
+import com.practice.filmorate.exceptions.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse validationHandle(final ValidationException e){
+        return new ErrorResponse("Ошибка валидации", e.getMessage());
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse objectFoundHandle(final NotFoundException e){
+        return new ErrorResponse("Объект не найден", e.getMessage());
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse RunTimeExceptionHandle(final RuntimeException e){
+        return new ErrorResponse("Cервер не может обработать запрос", e.getMessage());
+    }
+
+}
